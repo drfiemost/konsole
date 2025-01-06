drfiemost/konsole

commit 30b9b48b5580e5ea733ff841a362d6192091620f

Apply changes from upstream
https://github.com/KDE/konsole/commit/392c7d0fcc52a386358d4f6702bc3dcab4983e5e
https://github.com/KDE/konsole/commit/0d383d4355a520ec9dbf61f7eb5cd5a1f2af62fe

src/ViewManager.cpp
@@ -434,7 +434,7 @@ void ViewManager::splitView(Qt::Orientation orientation)
     // container and create a new view for that session in a new container
     foreach(QWidget* view,  _viewSplitter->activeContainer()->views()) {
         Session* session = _sessionMap[qobject_cast<TerminalDisplay*>(view)];
-        TerminalDisplay* display = createTerminalDisplay(session);
+        TerminalDisplay* display = createTerminalDisplay();
         const Profile::Ptr profile = SessionManager::instance()->sessionProfile(session);
         applyProfileToView(display, profile);
         ViewProperties* properties = createController(session, display);
@@ -552,7 +552,7 @@ void ViewManager::createView(Session* session, ViewContainer* container, int ind
     // Use Qt::UniqueConnection to avoid duplicate connection
     connect(session, SIGNAL(finished()), this, SLOT(sessionFinished()), Qt::UniqueConnection);
 
-    TerminalDisplay* display = createTerminalDisplay(session);
+    TerminalDisplay* display = createTerminalDisplay();
     const Profile::Ptr profile = SessionManager::instance()->sessionProfile(session);
     applyProfileToView(display, profile);
 
@@ -781,15 +781,14 @@ void ViewManager::viewDestroyed(QWidget* view)
 //        emit unplugController(_pluggedController);
 }
 
-TerminalDisplay* ViewManager::createTerminalDisplay(Session* session)
+TerminalDisplay* ViewManager::createTerminalDisplay()
 {
     TerminalDisplay* display = new TerminalDisplay(0);
-    display->setRandomSeed(session->sessionId() * 31);
 
     return display;
 }
 
-const ColorScheme* ViewManager::colorSchemeForProfile(const Profile::Ptr profile)
+const ColorScheme* ViewManager::colorSchemeForProfile(const Profile::Ptr &profile)
 {
     const ColorScheme* colorScheme = ColorSchemeManager::instance()->
                                      findColorScheme(profile->colorScheme());
@@ -800,95 +799,12 @@ const ColorScheme* ViewManager::colorSchemeForProfile(const Profile::Ptr profile
     return colorScheme;
 }
 
-void ViewManager::applyProfileToView(TerminalDisplay* view , const Profile::Ptr profile)
+void ViewManager::applyProfileToView(TerminalDisplay* view , const Profile::Ptr &profile)
 {
     Q_ASSERT(profile);
 
+    view->applyProfile(profile);
     emit updateWindowIcon();
-
-    // load color scheme
-    ColorEntry table[TABLE_COLORS];
-    const ColorScheme* colorScheme = colorSchemeForProfile(profile);
-    colorScheme->getColorTable(table , view->randomSeed());
-    view->setColorTable(table);
-    view->setOpacity(colorScheme->opacity());
-    view->setWallpaper(colorScheme->wallpaper());
-
-    // load font
-    view->setAntialias(profile->antiAliasFonts());
-    view->setBoldIntense(profile->boldIntense());
-    view->setVTFont(profile->font());
-
-    // set scroll-bar position
-    int scrollBarPosition = profile->property<int>(Profile::ScrollBarPosition);
-
-    if (scrollBarPosition == Enum::ScrollBarLeft)
-        view->setScrollBarPosition(Enum::ScrollBarLeft);
-    else if (scrollBarPosition == Enum::ScrollBarRight)
-        view->setScrollBarPosition(Enum::ScrollBarRight);
-    else if (scrollBarPosition == Enum::ScrollBarHidden)
-        view->setScrollBarPosition(Enum::ScrollBarHidden);
-
-    bool scrollFullPage = profile->property<bool>(Profile::ScrollFullPage);
-    view->setScrollFullPage(scrollFullPage);
-
-    // show hint about terminal size after resizing
-    view->setShowTerminalSizeHint(profile->showTerminalSizeHint());
-
-    // terminal features
-    view->setBlinkingCursorEnabled(profile->blinkingCursorEnabled());
-    view->setBlinkingTextEnabled(profile->blinkingTextEnabled());
-
-    int tripleClickMode = profile->property<int>(Profile::TripleClickMode);
-    view->setTripleClickMode(Enum::TripleClickModeEnum(tripleClickMode));
-
-    view->setAutoCopySelectedText(profile->autoCopySelectedText());
-    view->setUnderlineLinks(profile->underlineLinksEnabled());
-    view->setControlDrag(profile->property<bool>(Profile::CtrlRequiredForDrag));
-    view->setBidiEnabled(profile->bidiRenderingEnabled());
-    view->setLineSpacing(profile->lineSpacing());
-    view->setTrimTrailingSpaces(profile->property<bool>(Profile::TrimTrailingSpacesInSelectedText));
-
-    view->setOpenLinksByDirectClick(profile->property<bool>(Profile::OpenLinksByDirectClickEnabled));
-
-    int middleClickPasteMode = profile->property<int>(Profile::MiddleClickPasteMode);
-    if (middleClickPasteMode == Enum::PasteFromX11Selection)
-        view->setMiddleClickPasteMode(Enum::PasteFromX11Selection);
-    else if (middleClickPasteMode == Enum::PasteFromClipboard)
-        view->setMiddleClickPasteMode(Enum::PasteFromClipboard);
-
-    // margin/center - these are hard-fixed ATM
-    view->setMargin(1);
-    view->setCenterContents(false);
-
-    // cursor shape
-    int cursorShape = profile->property<int>(Profile::CursorShape);
-
-    if (cursorShape == Enum::BlockCursor)
-        view->setKeyboardCursorShape(Enum::BlockCursor);
-    else if (cursorShape == Enum::IBeamCursor)
-        view->setKeyboardCursorShape(Enum::IBeamCursor);
-    else if (cursorShape == Enum::UnderlineCursor)
-        view->setKeyboardCursorShape(Enum::UnderlineCursor);
-
-    // cursor color
-    if (profile->useCustomCursorColor()) {
-        const QColor& cursorColor = profile->customCursorColor();
-        view->setKeyboardCursorColor(cursorColor);
-    } else {
-        // an invalid QColor is used to inform the view widget to
-        // draw the cursor using the default color( matching the text)
-        view->setKeyboardCursorColor(QColor());
-    }
-
-    // word characters
-    view->setWordCharacters(profile->wordCharacters());
-
-    // bell mode
-    view->setBellMode(profile->property<int>(Profile::BellMode));
-
-    // mouse wheel zoom
-    view->setMouseWheelZoom(profile->mouseWheelZoomEnabled());
 }
 
 void ViewManager::updateViewsForSession(Session* session)

src/ColorScheme.cpp
@@ -23,6 +23,7 @@
 #include "ColorScheme.h"
 
 // Qt
+#include <QApplication>
 #include <QtGui/QPainter>
 
 // KDE
@@ -193,13 +194,13 @@ void ColorScheme::setColorTableEntry(int index , const ColorEntry& entry)
 
     _table[index] = entry;
 }
-ColorEntry ColorScheme::colorEntry(int index , uint randomSeed) const
+ColorEntry ColorScheme::colorEntry(int index , uint sessionId) const
 {
     Q_ASSERT(index >= 0 && index < TABLE_COLORS);
 
     ColorEntry entry = colorTable()[index];
 
-    if (randomSeed == 0 || _randomTable == 0 || _randomTable[index].isNull()) {
+    if (sessionId == 0 || _randomTable == 0 || _randomTable[index].isNull()) {
         return entry;
     }
 
@@ -209,7 +210,7 @@ ColorEntry ColorScheme::colorEntry(int index , uint randomSeed) const
     // 32-bit Mersenne Twister
     // Can't use default_random_engine, because in GCC this maps to
     // minstd_rand0 which always gives us 0 on the first number.
-    std::mt19937 randomEngine(randomSeed);
+    std::mt19937 randomEngine(sessionId | qApp->applicationPid() << 10);
 
     int hueDifference = 0;
     if (range.hue) {
@@ -239,10 +240,10 @@ ColorEntry ColorScheme::colorEntry(int index , uint randomSeed) const
 
     return entry;
 }
-void ColorScheme::getColorTable(ColorEntry* table , uint randomSeed) const
+void ColorScheme::getColorTable(ColorEntry* table , uint sessionId) const
 {
     for (int i = 0 ; i < TABLE_COLORS ; i++)
-        table[i] = colorEntry(i, randomSeed);
+        table[i] = colorEntry(i, sessionId);
 }
 bool ColorScheme::randomizedBackgroundColor() const
 {

src/ColorScheme.h
@@ -104,17 +104,17 @@ public:
      *
      * @param table Array into which the color entries for this color scheme
      * are copied.
-     * @param randomSeed Color schemes may allow certain colors in their
+     * @param sessionId Color schemes may allow certain colors in their
      * palette to be randomized.  The seed is used to pick the random color.
      */
-    void getColorTable(ColorEntry* table, uint randomSeed = 0) const;
+    void getColorTable(ColorEntry* table, uint sessionId = 0) const;
 
     /**
      * Retrieves a single color entry from the table.
      *
      * See getColorTable()
      */
-    ColorEntry colorEntry(int index , uint randomSeed = 0) const;
+    ColorEntry colorEntry(int index , uint sessionId = 0) const;
 
     /**
      * Convenience method.  Returns the

src/TerminalDisplay.cpp
@@ -71,6 +71,8 @@
 #include "TerminalDisplayAccessible.h"
 #include "SessionManager.h"
 #include "Session.h"
+#include "Profile.h"
+#include "ViewManager.h" // for colorSchemeForProfile. // TODO: Rewrite this.
 
 using namespace Konsole;
 
@@ -315,7 +317,6 @@ TerminalDisplay::TerminalDisplay(QWidget* parent)
     , _usedLines(1)
     , _usedColumns(1)
     , _image(0)
-    , _randomSeed(0)
     , _resizing(false)
     , _showTerminalSizeHint(true)
     , _bidiEnabled(false)
@@ -895,15 +896,6 @@ void TerminalDisplay::drawPrinterFriendlyTextFragment(QPainter& painter,
     drawCharacters(painter, rect, text, &print_style, false);
 
     painter.restore();
-}
-
-void TerminalDisplay::setRandomSeed(uint randomSeed)
-{
-    _randomSeed = randomSeed;
-}
-uint TerminalDisplay::randomSeed() const
-{
-    return _randomSeed;
 }
 
 // scrolls the image by 'lines', down if lines > 0 or up otherwise.
@@ -2445,7 +2437,7 @@ void TerminalDisplay::wheelEvent(QWheelEvent* ev)
     const int delta = ev->delta();
 
     // ctrl+<wheel> for zooming, like in konqueror and firefox
-    if ((modifiers & Qt::ControlModifier) && mouseWheelZoom()) {
+    if ((modifiers & Qt::ControlModifier) && _mouseWheelZoom) {
         if (delta > 0) {
             // wheel-up for increasing font size
             increaseFontSize();
@@ -3398,6 +3390,93 @@ bool AutoScrollHandler::eventFilter(QObject* watched, QEvent* event)
     };
 
     return false;
+}
+
+void TerminalDisplay::applyProfile(const Profile::Ptr &profile)
+{
+    // load color scheme
+    ColorEntry table[TABLE_COLORS];
+    _colorScheme = ViewManager::colorSchemeForProfile(profile);
+    _colorScheme->getColorTable(table, sessionController()->session()->sessionId());
+    setColorTable(table);
+    setOpacity(_colorScheme->opacity());
+    setWallpaper(_colorScheme->wallpaper());
+
+    // load font
+    _antialiasText = profile->antiAliasFonts();
+    _boldIntense = profile->boldIntense();
+    setVTFont(profile->font());
+
+    // set scroll-bar position
+    int scrollBarPosition = profile->property<int>(Profile::ScrollBarPosition);
+
+    if (scrollBarPosition == Enum::ScrollBarLeft)
+        setScrollBarPosition(Enum::ScrollBarLeft);
+    else if (scrollBarPosition == Enum::ScrollBarRight)
+        setScrollBarPosition(Enum::ScrollBarRight);
+    else if (scrollBarPosition == Enum::ScrollBarHidden)
+        setScrollBarPosition(Enum::ScrollBarHidden);
+
+    bool scrollFullPage = profile->property<bool>(Profile::ScrollFullPage);
+    setScrollFullPage(scrollFullPage);
+
+    // show hint about terminal size after resizing
+    _showTerminalSizeHint = profile->showTerminalSizeHint();
+
+    // terminal features
+    setBlinkingCursorEnabled(profile->blinkingCursorEnabled());
+    setBlinkingTextEnabled(profile->blinkingTextEnabled());
+
+    int tripleClickMode = profile->property<int>(Profile::TripleClickMode);
+    _tripleClickMode = Enum::TripleClickModeEnum(tripleClickMode);
+
+    setAutoCopySelectedText(profile->autoCopySelectedText());
+    setUnderlineLinks(profile->underlineLinksEnabled());
+    _ctrlRequiredForDrag = profile->property<bool>(Profile::CtrlRequiredForDrag);
+    _bidiEnabled = profile->bidiRenderingEnabled();
+    setLineSpacing(profile->lineSpacing());
+    _trimTrailingSpaces = profile->property<bool>(Profile::TrimTrailingSpacesInSelectedText);
+
+    _openLinksByDirectClick = profile->property<bool>(Profile::OpenLinksByDirectClickEnabled);
+
+    int middleClickPasteMode = profile->property<int>(Profile::MiddleClickPasteMode);
+    if (middleClickPasteMode == Enum::PasteFromX11Selection)
+        setMiddleClickPasteMode(Enum::PasteFromX11Selection);
+    else if (middleClickPasteMode == Enum::PasteFromClipboard)
+        setMiddleClickPasteMode(Enum::PasteFromClipboard);
+
+    // margin/center - these are hard-fixed ATM
+    setMargin(1);
+    setCenterContents(false);
+
+    // cursor shape
+    int cursorShape = profile->property<int>(Profile::CursorShape);
+
+    if (cursorShape == Enum::BlockCursor)
+        setKeyboardCursorShape(Enum::BlockCursor);
+    else if (cursorShape == Enum::IBeamCursor)
+        setKeyboardCursorShape(Enum::IBeamCursor);
+    else if (cursorShape == Enum::UnderlineCursor)
+        setKeyboardCursorShape(Enum::UnderlineCursor);
+
+    // cursor color
+    if (profile->useCustomCursorColor()) {
+        const QColor& cursorColor = profile->customCursorColor();
+        setKeyboardCursorColor(cursorColor);
+    } else {
+        // an invalid QColor is used to inform the view widget to
+        // draw the cursor using the default color( matching the text)
+        setKeyboardCursorColor(QColor());
+    }
+
+    // word characters
+    setWordCharacters(profile->wordCharacters());
+
+    // bell mode
+    setBellMode(profile->property<int>(Profile::BellMode));
+
+    // mouse wheel zoom
+    _mouseWheelZoom = profile->mouseWheelZoomEnabled() ;
 }
 
 #include "TerminalDisplay.moc"

src/TerminalDisplay.h
@@ -32,6 +32,7 @@
 #include "ScreenWindow.h"
 #include "ColorScheme.h"
 #include "Enumeration.h"
+#include "Profile.h"
 
 class QDrag;
 class QDragEnterEvent;
@@ -51,6 +52,7 @@ namespace Konsole
 class FilterChain;
 class TerminalImageFilterChain;
 class SessionController;
+
 /**
  * A widget which displays output from a terminal emulation and sends input keypresses and mouse activity
  * to the terminal.
@@ -69,20 +71,12 @@ public:
     explicit TerminalDisplay(QWidget* parent = 0);
     virtual ~TerminalDisplay();
 
+    void applyProfile(const Profile::Ptr& profile);
+
     /** Returns the terminal color palette used by the display. */
     const ColorEntry* colorTable() const;
     /** Sets the terminal color palette used by the display. */
     void setColorTable(const ColorEntry table[]);
-    /**
-     * Sets the seed used to generate random colors for the display
-     * (in color schemes that support them).
-     */
-    void setRandomSeed(uint seed);
-    /**
-     * Returns the seed used to generate random colors for the display
-     * (in color schemes that support them).
-     */
-    uint randomSeed() const;
 
     /** Sets the opacity of the terminal display. */
     void setOpacity(qreal opacity);
@@ -95,9 +89,6 @@ public:
      * is shown on the left or right side of the display.
      */
     void setScrollBarPosition(Enum::ScrollBarPositionEnum position);
-    Enum::ScrollBarPositionEnum scrollBarPosition() const {
-        return _scrollbarLocation;
-    }
 
     /**
      * Sets the current position and range of the display's scroll bar.
@@ -147,29 +138,9 @@ public:
 
     /** Specifies whether or not the cursor can blink. */
     void setBlinkingCursorEnabled(bool blink);
-    /** Returns true if the cursor is allowed to blink or false otherwise. */
-    bool blinkingCursorEnabled() const {
-        return _allowBlinkingCursor;
-    }
 
     /** Specifies whether or not text can blink. */
     void setBlinkingTextEnabled(bool blink);
-
-    void setControlDrag(bool enable) {
-        _ctrlRequiredForDrag = enable;
-    }
-    bool ctrlRequiredForDrag() const {
-        return _ctrlRequiredForDrag;
-    }
-
-    /** Sets how the text is selected when the user triple clicks within the display. */
-    void setTripleClickMode(Enum::TripleClickModeEnum mode) {
-        _tripleClickMode = mode;
-    }
-    /** See setTripleClickSelectionMode() */
-    Enum::TripleClickModeEnum tripleClickMode() const {
-        return _tripleClickMode;
-    }
 
     /**
      * Specifies whether links and email addresses should be underlined when
@@ -184,35 +155,6 @@ public:
      */
     bool getUnderlineLinks() const {
         return _underlineLinks;
-    }
-
-    /**
-     * Specifies whether links and email addresses should be opened when
-     * clicked with the mouse. Defaults to false.
-     */
-    void setOpenLinksByDirectClick(bool value) {
-        _openLinksByDirectClick = value;
-    }
-    /**
-     * Returns true if links and email addresses should be opened when
-     * clicked with the mouse.
-     */
-    bool getOpenLinksByDirectClick() const {
-        return _openLinksByDirectClick;
-    }
-
-    /**
-     * Sets whether trailing spaces should be trimmed in selected text.
-     */
-    void setTrimTrailingSpaces(bool enabled) {
-        _trimTrailingSpaces = enabled;
-    }
-
-    /**
-     * Returns true if trailing spaces should be trimmed in selected text.
-     */
-    bool trimTrailingSpaces() const {
-        return _trimTrailingSpaces;
     }
 
     void setLineSpacing(uint);
@@ -310,15 +252,6 @@ public:
      * of a word ( in addition to letters and numbers ).
      */
     void setWordCharacters(const QString& wc);
-    /**
-     * Returns the characters which are considered part of a word for the
-     * purpose of selecting words in the display with the mouse.
-     *
-     * @see setWordCharacters()
-     */
-    QString wordCharacters() const {
-        return _wordCharacters;
-    }
 
     /**
      * Sets the type of effect used to alert the user when a 'bell' occurs in the
@@ -338,22 +271,6 @@ public:
 
     /** Play a visual bell for prompt or warning. */
     void visualBell();
-
-    /**
-     * Specified whether zoom terminal on Ctrl+mousewheel  is enabled or not.
-     * Defaults to enabled.
-     */
-    void setMouseWheelZoom(bool value) {
-        _mouseWheelZoom = value;
-    };
-    /**
-     * Returns the whether zoom terminal on Ctrl+mousewheel is enabled.
-     *
-     * See setMouseWheelZoom()
-     */
-    bool mouseWheelZoom() {
-        return _mouseWheelZoom;
-    };
 
     /**
      * Reimplemented.  Has no effect.  Use setVTFont() to change the font
@@ -377,65 +294,6 @@ public:
 
     /** Decreases the font size */
     void decreaseFontSize();
-
-    /**
-     * Specified whether anti-aliasing of text in the terminal display
-     * is enabled or not.  Defaults to enabled.
-     */
-    void setAntialias(bool value) {
-        _antialiasText = value;
-    }
-    /**
-     * Returns true if anti-aliasing of text in the terminal is enabled.
-     */
-    bool antialias() const {
-        return _antialiasText;
-    }
-
-    /**
-     * Specifies whether characters with intense colors should be rendered
-     * as bold. Defaults to true.
-     */
-    void setBoldIntense(bool value) {
-        _boldIntense = value;
-    }
-    /**
-     * Returns true if characters with intense colors are rendered in bold.
-     */
-    bool getBoldIntense() const {
-        return _boldIntense;
-    }
-
-    /**
-     * Sets whether or not the current height and width of the
-     * terminal in lines and columns is displayed whilst the widget
-     * is being resized.
-     */
-    void setShowTerminalSizeHint(bool on) {
-        _showTerminalSizeHint = on;
-    }
-    /**
-     * Returns whether or not the current height and width of
-     * the terminal in lines and columns is displayed whilst the widget
-     * is being resized.
-     */
-    bool showTerminalSizeHint() const {
-        return _showTerminalSizeHint;
-    }
-
-    /**
-     * Sets the status of the BiDi rendering inside the terminal display.
-     * Defaults to disabled.
-     */
-    void setBidiEnabled(bool set) {
-        _bidiEnabled = set;
-    }
-    /**
-     * Returns the status of the BiDi rendering in this widget.
-     */
-    bool isBidiEnabled() const {
-        return _bidiEnabled;
-    }
 
     /**
      * Sets the terminal screen section which is displayed in this widget.
@@ -572,6 +430,16 @@ public slots:
      * Sets whether the contents are centered between the margins.
      */
     void setCenterContents(bool enable);
+
+    ColorScheme const *colorScheme() const
+    {
+        return _colorScheme;
+    }
+
+    Enum::ScrollBarPositionEnum scrollBarPosition() const
+    {
+        return _scrollbarLocation;
+    }
 
     void updateFilters();
 
@@ -814,7 +682,6 @@ private:
     QVector<LineProperty> _lineProperties;
 
     ColorEntry _colorTable[TABLE_COLORS];
-    uint _randomSeed;
 
     bool _resizing;
     bool _showTerminalSizeHint;
@@ -872,6 +739,7 @@ private:
 
     QRgb _blendColor;
 
+    ColorScheme const* _colorScheme;
     ColorSchemeWallpaper::Ptr _wallpaper;
 
     // list of filters currently applied to the display.  used for links and

src/ViewManager.h
@@ -84,12 +84,6 @@ public:
     void createView(Session* session);
 
     /**
-     * Applies the view-specific settings associated with specified @p profile
-     * to the terminal display @p view.
-     */
-    void applyProfileToView(TerminalDisplay* view , const Profile::Ptr profile);
-
-    /**
      * Return the main widget for the view manager which
      * holds all of the views managed by this ViewManager instance.
      */
@@ -176,6 +170,10 @@ public:
 
     /** Returns a list of sessions in this ViewManager */
     QList<Session*> sessions() { return _sessionMap.values(); }
+
+    void applyProfileToView(TerminalDisplay *view, const Profile::Ptr &profile);
+
+    static const ColorScheme *colorSchemeForProfile(const Profile::Ptr &profile);
 
 signals:
     /** Emitted when the last view is removed from the view manager */
@@ -343,7 +341,6 @@ private slots:
 
 private:
     void createView(Session* session, ViewContainer* container, int index);
-    static const ColorScheme* colorSchemeForProfile(const Profile::Ptr profile);
 
     void setupActions();
     void focusActiveView();
@@ -359,9 +356,7 @@ private:
     void removeContainer(ViewContainer* container);
 
     // creates a new terminal display
-    // the 'session' is used so that the terminal display's random seed
-    // can be set to something which depends uniquely on that session
-    TerminalDisplay* createTerminalDisplay(Session* session = 0);
+    TerminalDisplay* createTerminalDisplay();
 
     // creates a new controller for a session/display pair which provides the menu
     // actions associated with that view, and exposes basic information

commit ccf3487a19599a5e4aba00429d690d20037737cf

Fix crash introduced with 30b9b48

src/ViewManager.cpp
@@ -553,6 +553,8 @@ void ViewManager::createView(Session* session, ViewContainer* container, int ind
     connect(session, SIGNAL(finished()), this, SLOT(sessionFinished()), Qt::UniqueConnection);
 
     TerminalDisplay* display = createTerminalDisplay();
+    ViewProperties* properties = createController(session, display);
+
     const Profile::Ptr profile = SessionManager::instance()->sessionProfile(session);
     applyProfileToView(display, profile);
 
@@ -571,7 +573,6 @@ void ViewManager::createView(Session* session, ViewContainer* container, int ind
     }
 
     display->setSize(preferredSize.width(), preferredSize.height() + heightAdjustment);
-    ViewProperties* properties = createController(session, display);
 
     _sessionMap[display] = session;
     container->addView(display, properties, index);