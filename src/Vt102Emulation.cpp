drfiemost/konsole

commit be2018bef113c346080b4ef50f22af906b3ba704

Apply changes from upstream
https://github.com/KDE/konsole/commit/a2ad2d70d45d7f7c91b01217c7a3cd391bdca41e
https://github.com/KDE/konsole/commit/033e20319f76be1e53d9c6be3c9d3586fd5abc8f
https://github.com/KDE/konsole/commit/8f02c3ed9b9c76ce318d70567fbbfa55527dadf1
https://github.com/KDE/konsole/commit/03b0026e202527348c01f4a90e41b917af24d1dd

src/Vt102Emulation.cpp
@@ -66,10 +66,10 @@ unsigned short Konsole::vt100_graphics[32] = {
 
 Vt102Emulation::Vt102Emulation()
     : Emulation(),
-      _titleUpdateTimer(new QTimer(this))
+      _sessionAttributesUpdateTimer(new QTimer(this))
 {
-    _titleUpdateTimer->setSingleShot(true);
-    QObject::connect(_titleUpdateTimer , &QTimer::timeout , this , &Konsole::Vt102Emulation::updateTitle);
+    _sessionAttributesUpdateTimer->setSingleShot(true);
+    QObject::connect(_sessionAttributesUpdateTimer , &QTimer::timeout , this , &Konsole::Vt102Emulation::updateSessionAttributes);
 
     initTokenizer();
     reset();
@@ -374,7 +374,7 @@ void Vt102Emulation::receiveChar(uint cc)
     if (lec(1,0,ESC)) { return; }
     if (lec(1,0,ESC+128)) { s[0] = ESC; receiveChar('['); return; }
     if (les(2,1,GRP)) { return; }
-    if (Xte         ) { processWindowAttributeRequest(); resetTokenizer(); return; }
+    if (Xte         ) { processSessionAttributeRequest(); resetTokenizer(); return; }
     if (Xpe         ) { return; }
     if (lec(3,2,'?')) { return; }
     if (lec(3,2,'>')) { return; }
@@ -454,10 +454,10 @@ void Vt102Emulation::receiveChar(uint cc)
   }
 }
 
-void Vt102Emulation::processWindowAttributeRequest()
+void Vt102Emulation::processSessionAttributeRequest()
 {
   // Describes the window or terminal session attribute to change
-  // See Session::UserTitleChange for possible values
+  // See Session::SessionAttributes for possible values
   int attribute = 0;
   int i;
   for (i = 2; i < tokenBufferPos     &&
@@ -483,18 +483,18 @@ void Vt102Emulation::processWindowAttributeRequest()
       return;
   }
 
-  _pendingTitleUpdates[attribute] = value;
-  _titleUpdateTimer->start(20);
+  _pendingSessionAttributesUpdates[attribute] = value;
+  _sessionAttributesUpdateTimer->start(20);
 }
 
-void Vt102Emulation::updateTitle()
+void Vt102Emulation::updateSessionAttributes()
 {
-    QListIterator<int> iter( _pendingTitleUpdates.keys() );
+    QListIterator<int> iter( _pendingSessionAttributesUpdates.keys() );
     while (iter.hasNext()) {
         int arg = iter.next();
-        emit titleChanged( arg , _pendingTitleUpdates[arg] );
+        emit sessionAttributeChanged( arg , _pendingSessionAttributesUpdates[arg] );
     }
-    _pendingTitleUpdates.clear();
+    _pendingSessionAttributesUpdates.clear();
 }
 
 // Interpreting Codes ---------------------------------------------------------
@@ -1323,7 +1323,7 @@ void Vt102Emulation::setMode(int m)
     case MODE_Mouse1001:
     case MODE_Mouse1002:
     case MODE_Mouse1003:
-        emit programUsesMouseChanged(false);
+        emit programRequestsMouseTracking(false);
         break;
     case MODE_Mouse1007:
         emit enableAlternateScrolling(true);
@@ -1359,7 +1359,7 @@ void Vt102Emulation::resetMode(int m)
     case MODE_Mouse1001 :
     case MODE_Mouse1002 :
     case MODE_Mouse1003 :
-        emit programUsesMouseChanged(true);
+        emit programRequestsMouseTracking(true);
         break;
     case MODE_Mouse1007:
         emit enableAlternateScrolling(false);

src/Emulation.cpp
@@ -39,7 +39,7 @@ Emulation::Emulation() :
     _codec(nullptr),
     _decoder(nullptr),
     _keyTranslator(nullptr),
-    _usesMouse(false),
+    _usesMouseTracking(false),
     _bracketedPasteMode(false),
     _bulkTimer1(new QTimer(this)),
     _bulkTimer2(new QTimer(this)),
@@ -54,20 +54,20 @@ Emulation::Emulation() :
     QObject::connect(&_bulkTimer2, &QTimer::timeout, this, &Konsole::Emulation::showBulk);
 
     // listen for mouse status changes
-    connect(this , &Konsole::Emulation::programUsesMouseChanged ,
-            this, &Konsole::Emulation::usesMouseChanged);
-    connect(this , &Konsole::Emulation::programBracketedPasteModeChanged ,
+    connect(this , &Konsole::Emulation::programRequestsMouseTracking,
+            this, &Konsole::Emulation::setUsesMouseTracking);
+    connect(this , &Konsole::Emulation::programBracketedPasteModeChanged,
             this, &Konsole::Emulation::bracketedPasteModeChanged);
 }
 
-bool Emulation::programUsesMouse() const
+bool Emulation::programUsesMouseTracking() const
 {
-    return _usesMouse;
+    return _usesMouseTracking;
 }
 
-void Emulation::usesMouseChanged(bool usesMouse)
+void Emulation::setUsesMouseTracking(bool usesMouseTracking)
 {
-    _usesMouse = usesMouse;
+    _usesMouseTracking = usesMouseTracking;
 }
 
 bool Emulation::programBracketedPasteMode() const

src/Emulation.h
@@ -208,13 +208,15 @@ public:
     virtual void reset() = 0;
 
     /**
-     * Returns true if the active terminal program wants
-     * mouse input events.
+     * Returns true if the active terminal program is interested in Mouse
+     * Tracking events.
      *
-     * The programUsesMouseChanged() signal is emitted when this
-     * changes.
+     * The programRequestsMouseTracking() signal is emitted when a program
+     * indicates it's interested in Mouse Tracking events.
+     *
+     * See MODE_Mouse100{0,1,2,3} in Vt102Emulation.
      */
-    bool programUsesMouse() const;
+    bool programUsesMouseTracking() const;
 
     bool programBracketedPasteMode() const;
 
@@ -306,13 +308,16 @@ signals:
     void changeTabTextColorRequest(int color);
 
     /**
-     * This is emitted when the program running in the shell indicates whether or
-     * not it is interested in mouse events.
+     * This is emitted when the program (typically editors and other full-screen
+     * applications, ones that take up the whole terminal display), running in
+     * the terminal indicates whether or not it is interested in Mouse Tracking
+     * events. This is an XTerm extension, for more information have a look at:
+     * http://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-Mouse-Tracking
      *
-     * @param usesMouse This will be true if the program wants to be informed about
-     * mouse events or false otherwise.
+     * @param usesMouseTracking This will be true if the program is interested
+     * in Mouse Tracking events or false otherwise.
      */
-    void programUsesMouseChanged(bool usesMouse);
+    void programRequestsMouseTracking(bool usesMouseTracking);
 
     void enableAlternateScrolling(bool enable);
 
@@ -333,38 +338,37 @@ signals:
     void outputChanged();
 
     /**
-     * Emitted when the program running in the terminal wishes to update the
-     * session's title.  This also allows terminal programs to customize other
-     * aspects of the terminal emulation display.
+     * Emitted when the program running in the terminal wishes to update
+     * certain session attributes. This allows terminal programs to customize
+     * certain aspects of the terminal emulation display.
      *
      * This signal is emitted when the escape sequence "\033]ARG;VALUE\007"
-     * is received in the input string, where ARG is a number specifying what
-     * should change and VALUE is a string specifying the new value.
+     * is received in an input string, where ARG is a number specifying
+     * what should be changed and VALUE is a string specifying the new value.
      *
-     * TODO:  The name of this method is not very accurate since this method
-     * is used to perform a whole range of tasks besides just setting
-     * the user-title of the session.
-     *
-     * @param title Specifies what to change.
+     * @param attribute Specifies which session attribute to change:
      * <ul>
-     * <li>0 - Set window icon text and session title to @p newTitle</li>
-     * <li>1 - Set window icon text to @p newTitle</li>
-     * <li>2 - Set session title to @p newTitle</li>
-     * <li>11 - Set the session's default background color to @p newTitle,
-     *         where @p newTitle can be an HTML-style string ("#RRGGBB") or a named
-     *         color (eg 'red', 'blue').
-     *         See http://doc.trolltech.com/4.2/qcolor.html#setNamedColor for more
-     *         details.
+     * <li> 0  - Set window icon text and session title to @p newValue</li>
+     * <li> 1  - Set window icon text to @p newValue</li>
+     * <li> 2  - Set session title to @p newValue</li>
+     * <li> 11 - Set the session's default background color to @p newValue,
+     *      where @p newValue can be an HTML-style string ("#RRGGBB") or a
+     *      named color (e.g. 'red', 'blue'). For more details see:
+     *      https://doc.qt.io/qt-5/qcolor.html#setNamedColor
      * </li>
-     * <li>31 - Supposedly treats @p newTitle as a URL and opens it (NOT IMPLEMENTED)</li>
-     * <li>32 - Sets the icon associated with the session.  @p newTitle is the name
-     *    of the icon to use, which can be the name of any icon in the current KDE icon
-     *    theme (eg: 'konsole', 'kate', 'folder_home')</li>
+     * <li> 31 - Supposedly treats @p newValue as a URL and opens it (NOT
+     *      IMPLEMENTED)
+     * </li>
+     * <li> 32 - Sets the icon associated with the session. @p newValue
+     *      is the name of the icon to use, which can be the name of any
+     *      icon in the current KDE icon theme (eg: 'konsole', 'kate',
+     *      'folder_home')
+     * </li>
      * </ul>
-     * @param newTitle Specifies the new title
+     * @param newValue Specifies the new value of the session attribute
      */
 
-    void titleChanged(int title, const QString& newTitle);
+    void sessionAttributeChanged(int attribute, const QString &newValue);
 
     /**
      * Emitted when the terminal emulator's size has changed
@@ -494,14 +498,14 @@ private slots:
     // view
     void showBulk();
 
-    void usesMouseChanged(bool usesMouse);
+    void setUsesMouseTracking(bool usesMouseTracking);
 
     void bracketedPasteModeChanged(bool bracketedPasteMode);
 
 private:
     Q_DISABLE_COPY(Emulation)
 
-    bool _usesMouse;
+    bool _usesMouseTracking;
     bool _bracketedPasteMode;
     QTimer _bulkTimer1;
     QTimer _bulkTimer2;

src/Profile.h
@@ -259,16 +259,19 @@ public:
          * increases/decreases the terminal font size.
          */
         MouseWheelZoomEnabled,
-        /** (bool) Specifies whether emulated up/down key presses are sent
-         * for mouse scroll wheel events, to programs using the Alternate
-         * Screen buffer if those programs indicate they're not interested
-         * in mouse events (check _usesMouse in the Emulation header); this
-         * is mainly for the benefit of programs that are never interested
-         * in mouse events (e.g. less).
-         * Some programs, such as vim, can tell the terminal whether they
-         * are interested in mouse events or not. Consequently, this option
-         * affects vim only if it indicates that it's not interested in
-         * mouse events.
+        /** (bool) Specifies whether emulated up/down key press events are
+         * sent, for mouse scroll wheel events, to programs using the
+         * Alternate Screen buffer; this is mainly for the benefit of
+         * programs that don't natively support mouse scroll events, e.g.
+         * less.
+         *
+         * This also works for scrolling in applications that support Mouse
+         * Tracking events but don't indicate they're interested in those
+         * events; for example, when vim doesn't indicate it's interested
+         * in Mouse Tracking events (i.e. when the mouse is in Normal
+         * (not Visual) mode): http://vimdoc.sourceforge.net/htmldoc/intro.html#Normal
+         * mouse wheel scroll events will send up/down key press events.
+         *
          * Default value is true.
          * See also, MODE_Mouse1007 in the Emulation header, which toggles
          * Alternate Scrolling with escape sequences.

src/Session.cpp
@@ -97,10 +97,8 @@ Session::Session(QObject* parent) :
     //create emulation backend
     _emulation = new Vt102Emulation();
 
-connect(_emulation, &Konsole::Emulation::titleChanged,
-            this, &Konsole::Session::setUserTitle);
-    connect(_emulation, &Konsole::Emulation::stateSet,
-            this, &Konsole::Session::activityStateSet);
+    connect(_emulation, &Konsole::Emulation::sessionAttributeChanged, this, &Konsole::Session::setSessionAttribute);
+    connect(_emulation, &Konsole::Emulation::stateSet, this, &Konsole::Session::activityStateSet);
     connect(_emulation, &Konsole::Emulation::zmodemDownloadDetected, this, &Konsole::Session::fireZModemDownloadDetected);
     connect(_emulation, &Konsole::Emulation::zmodemUploadDetected, this, &Konsole::Session::fireZModemUploadDetected);
     connect(_emulation, &Konsole::Emulation::changeTabTextColorRequest,
@@ -298,12 +296,11 @@ void Session::addView(TerminalDisplay* widget)
     connect(widget, &Konsole::TerminalDisplay::sendStringToEmu,
             _emulation, &Konsole::Emulation::sendString);
 
-    // allow emulation to notify view when the foreground process
-    // indicates whether or not it is interested in mouse signals
-    connect(_emulation, &Konsole::Emulation::programUsesMouseChanged,
-            widget, &Konsole::TerminalDisplay::setUsesMouse);
+    // allow emulation to notify the view when the foreground process
+    // indicates whether or not it is interested in Mouse Tracking events
+    connect(_emulation, &Konsole::Emulation::programRequestsMouseTracking, widget, &Konsole::TerminalDisplay::setUsesMouseTracking);
 
-    widget->setUsesMouse(_emulation->programUsesMouse());
+    widget->setUsesMouseTracking(_emulation->programUsesMouseTracking());
 
     connect(_emulation, &Konsole::Emulation::enableAlternateScrolling, widget, &Konsole::TerminalDisplay::setAlternateScrolling);
 
@@ -493,9 +490,10 @@ void Session::run()
     emit started();
 }
 
-void Session::setUserTitle(int what, const QString& caption)
+void Session::setSessionAttribute(int what, const QString& caption)
 {
-    //set to true if anything is actually changed (eg. old _nameTitle != new _nameTitle )
+    // set to true if anything has actually changed
+    // eg. old _nameTitle != new _nameTitle
     bool modified = false;
 
     if ((what == IconNameAndWindowTitle) || (what == WindowTitle)) {
@@ -562,7 +560,7 @@ void Session::setUserTitle(int what, const QString& caption)
     }
 
     if (modified)
-        emit titleChanged();
+        emit sessionAttributeChanged();
 }
 
 QString Session::userTitle() const
@@ -905,7 +903,7 @@ void Session::done(int exitCode, QProcess::ExitStatus exitStatus)
 
     if (!_autoClose) {
         _userTitle = i18nc("@info:shell This session is done", "Finished");
-        emit titleChanged();
+        emit sessionAttributeChanged();
         return;
     }
 
@@ -980,7 +978,7 @@ void Session::setTitle(TitleRole role , const QString& newTitle)
         else if (role == DisplayedTitleRole)
             _displayTitle = newTitle;
 
-        emit titleChanged();
+        emit sessionAttributeChanged();
     }
 }
 
@@ -1159,7 +1157,7 @@ void Session::setIconName(const QString& iconName)
 {
     if (iconName != _iconName) {
         _iconName = iconName;
-        emit titleChanged();
+        emit sessionAttributeChanged();
     }
 }
 

src/Session.h
@@ -357,10 +357,11 @@ public:
     }
 
     /**
-      * Possible values of the @p what parameter for setUserTitle()
-      * See "Operating System Controls" section on http://rtfm.etla.org/xterm/ctlseq.html
+      * Possible values of the @p what parameter for setSessionAttribute().
+      * See the "Operating System Commands" section at:
+      * http://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-Operating-System-Commands
       */
-    enum UserTitleChange {
+    enum SessionAttributes {
         IconNameAndWindowTitle = 0,
         IconName               = 1,
         WindowTitle            = 2,
@@ -437,14 +438,15 @@ public slots:
     bool closeInForceWay();
 
     /**
-     * Changes the session title or other customizable aspects of the terminal
-     * emulation display. For a list of what may be changed see the
-     * Emulation::titleChanged() signal.
+     * Changes one of certain session attributes in the terminal emulation
+     * display. For a list of what may be changed see the
+     * Emulation::sessionAttributeChanged() signal.
      *
-     * @param what The feature being changed.  Value is one of UserTitleChange
+     * @param what The session attribute being changed, it is one of the
+     * SessionAttributes enum
      * @param caption The text part of the terminal command
      */
-    void setUserTitle(int what , const QString& caption);
+    void setSessionAttribute(int what , const QString& caption);
 
     /**
      * Enables monitoring for activity in the session.
@@ -598,8 +600,11 @@ signals:
      */
     void finished();
 
-    /** Emitted when the session's title has changed. */
-    void titleChanged();
+    /**
+     * Emitted when one of certain session attributes has been changed.
+     * See setSessionAttribute().
+     */
+    void sessionAttributeChanged();
 
     /**
      * Emitted when the activity state of this session changes.

src/SessionController.cpp
@@ -136,7 +136,7 @@ SessionController::SessionController(Session* session , TerminalDisplay* view, Q
     }
 
     setIdentifier(++_lastControllerId);
-    sessionTitleChanged();
+    sessionAttributeChanged();
 
     view->installEventFilter(this);
     view->setSessionController(this);
@@ -160,7 +160,7 @@ SessionController::SessionController(Session* session , TerminalDisplay* view, Q
     connect(_session.data(), &Konsole::Session::stateChanged, this,
             &Konsole::SessionController::sessionStateChanged);
     // listen to title and icon changes
-    connect(_session.data(), &Konsole::Session::titleChanged, this, &Konsole::SessionController::sessionTitleChanged);
+    connect(_session.data(), &Konsole::Session::sessionAttributeChanged, this, &Konsole::SessionController::sessionAttributeChanged);
 
     connect(this, &Konsole::SessionController::tabRenamedByUser,  _session,  &Konsole::Session::tabRenamedByUser);
 
@@ -1455,7 +1455,7 @@ void SessionController::updateSessionIcon()
         }
     }
 }
-void SessionController::sessionTitleChanged()
+void SessionController::sessionAttributeChanged()
 {
     if (_sessionIconName != _session->iconName()) {
         _sessionIconName = _session->iconName();

src/SessionController.h
@@ -265,7 +265,7 @@ private slots:
     void showDisplayContextMenu(const QPoint& position);
     void movementKeyFromSearchBarReceived(QKeyEvent *event);
     void sessionStateChanged(int state);
-    void sessionTitleChanged();
+    void sessionAttributeChanged();
     void searchTextChanged(const QString& text);
     void searchCompleted(bool success);
     void searchClosed(); // called when the user clicks on the

commit c5ced1391edc82eb8ad3047834a747410360b25f

Fix bug introduced with be2018b

src/Vt102Emulation.cpp
@@ -1323,7 +1323,7 @@ void Vt102Emulation::setMode(int m)
     case MODE_Mouse1001:
     case MODE_Mouse1002:
     case MODE_Mouse1003:
-        emit programRequestsMouseTracking(false);
+        emit programRequestsMouseTracking(true);
         break;
     case MODE_Mouse1007:
         emit enableAlternateScrolling(true);
@@ -1359,7 +1359,7 @@ void Vt102Emulation::resetMode(int m)
     case MODE_Mouse1001 :
     case MODE_Mouse1002 :
     case MODE_Mouse1003 :
-        emit programRequestsMouseTracking(true);
+        emit programRequestsMouseTracking(false);
         break;
     case MODE_Mouse1007:
         emit enableAlternateScrolling(false);