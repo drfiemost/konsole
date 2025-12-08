bdd1033ef35a4bf8bc0117485044eb579588307e
Leandro Nini <drfiemost@users.noreply.github.com>
2025-12-08 17:31:38 +0100

    Revert change https://github.com/KDE/konsole/commit/47199ba564628f8e7230ce3cea80f65aefe17dfa
    QRegExp don't support non-greedy matches

--- src/KeyboardTranslator.cpp
+++ src/KeyboardTranslator.cpp
@@ -364,7 +364,7 @@ QList<KeyboardTranslatorReader::Token> KeyboardTranslatorReader::tokenize(const 
     static QRegExp title("keyboard\\s+\"(.*)\"");
     // key line: key KeySequence : "output"
     // key line: key KeySequence : command
-    static QRegExp key("key\\s+(.+?)\\s*:\\s*(\"(.*)\"|\\w+)");
+    static QRegExp key("key\\s+([\\w\\+\\s\\-\\*\\.]+)\\s*:\\s*(\"(.*)\"|\\w+)");
 
     QList<Token> list;
     if (text.isEmpty()) {
