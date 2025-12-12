drfiemost/konsole

commit d524ded51a383da2e490e3072012a09db31a8373

Fix deprecated warnings

src/Screen.cpp
@@ -453,7 +453,7 @@ void Screen::getImage(Character* dest, int size, int startLine, int endLine) con
     Q_ASSERT(size >= mergedLines * _columns);
     Q_UNUSED(size);
 
-    const int linesInHistoryBuffer = qBound(0, _history->getLines() - startLine, mergedLines);
+    const int linesInHistoryBuffer = std::clamp(_history->getLines() - startLine, 0, mergedLines);
     const int linesInScreenBuffer = mergedLines - linesInHistoryBuffer;
 
     // copy _lines from history buffer
@@ -484,7 +484,7 @@ QVector<LineProperty> Screen::getLineProperties(int startLine , int endLine) con
     Q_ASSERT(endLine >= startLine && endLine < _history->getLines() + _lines);
 
     const int mergedLines = endLine - startLine + 1;
-    const int linesInHistory = qBound(0, _history->getLines() - startLine, mergedLines);
+    const int linesInHistory = std::clamp(_history->getLines() - startLine, 0, mergedLines);
     const int linesInScreen = mergedLines - linesInHistory;
 
     QVector<LineProperty> result(mergedLines);
@@ -1257,7 +1257,7 @@ int Screen::copyLineToStream(int line ,
         }
 
         // count cannot be any greater than length
-        count = qBound(0, count, length - start);
+        count = std::clamp(count, 0, length - start);
 
         Q_ASSERT(screenLine < _lineProperties.count());
         currentLineProperties |= _lineProperties[screenLine];

src/ScreenWindow.cpp
@@ -211,7 +211,7 @@ QPoint ScreenWindow::cursorPosition() const
 
 int ScreenWindow::currentLine() const
 {
-    return qBound(0, _currentLine, lineCount() - windowLines());
+    return std::clamp(_currentLine, 0, lineCount() - windowLines());
 }
 
 int ScreenWindow::currentResultLine() const
@@ -249,7 +249,7 @@ bool ScreenWindow::atEndOfOutput() const
 void ScreenWindow::scrollTo(int line)
 {
     int maxCurrentLineNumber = lineCount() - windowLines();
-    line = qBound(0, line, maxCurrentLineNumber);
+    line = std::clamp(line, 0, maxCurrentLineNumber);
 
     const int delta = line - _currentLine;
     _currentLine = line;

src/TerminalDisplay.cpp
@@ -2162,8 +2162,8 @@ void TerminalDisplay::extendSelection(const QPoint& position)
     // Adjust position within text area bounds.
     const QPoint oldpos = pos;
 
-    pos.setX(qBound(textBounds.left(), pos.x(), textBounds.right()));
-    pos.setY(qBound(textBounds.top(), pos.y(), textBounds.bottom()));
+    pos.setX(std::clamp(pos.x(), textBounds.left(), textBounds.right()));
+    pos.setY(std::clamp(pos.y(), textBounds.top(), textBounds.bottom()));
 
     if (oldpos.y() > textBounds.bottom()) {
         linesBeyondWidget = (oldpos.y() - textBounds.bottom()) / _fontHeight;

commit 60ce54848a8eb5451d87d9332b47f4f3615d62d5

Fix crash introduced with d524ded

src/Screen.cpp
@@ -462,7 +462,7 @@ void Screen::getImage(Character* dest, int size, int startLine, int endLine) con
     Q_ASSERT(size >= mergedLines * _columns);
     Q_UNUSED(size);
 
-    const int linesInHistoryBuffer = std::clamp(_history->getLines() - startLine, 0, mergedLines);
+    const int linesInHistoryBuffer = bound(_history->getLines() - startLine, 0, mergedLines);
     const int linesInScreenBuffer = mergedLines - linesInHistoryBuffer;
 
     // copy _lines from history buffer
@@ -494,7 +494,7 @@ QVector<LineProperty> Screen::getLineProperties(int startLine , int endLine) con
     Q_ASSERT(endLine >= startLine && endLine < _history->getLines() + _lines);
 
     const int mergedLines = endLine - startLine + 1;
-    const int linesInHistory = std::clamp(_history->getLines() - startLine, 0, mergedLines);
+    const int linesInHistory = bound(_history->getLines() - startLine, 0, mergedLines);
     const int linesInScreen = mergedLines - linesInHistory;
 
     QVector<LineProperty> result(mergedLines);
@@ -1258,7 +1258,7 @@ int Screen::copyLineToStream(int line ,
         }
 
         // count cannot be any greater than length
-        count = std::clamp(count, 0, length - start);
+        count = bound(count, 0, length - start);
 
         Q_ASSERT(screenLine < _lineProperties.count());
         currentLineProperties |= _lineProperties[screenLine];

src/Screen.h
@@ -49,6 +49,12 @@ class TerminalDisplay;
 class HistoryType;
 class HistoryScroll;
 
+template<typename T>
+const T &bound(const T &val, const T &min, const T &max)
+{
+    return std::max(min, std::min(max, val));
+}
+
 /**
     \brief An image of characters with associated attributes.
 

src/ScreenWindow.cpp
@@ -211,7 +211,7 @@ QPoint ScreenWindow::cursorPosition() const
 
 int ScreenWindow::currentLine() const
 {
-    return std::clamp(_currentLine, 0, std::max(0, lineCount() - windowLines()));
+    return bound(_currentLine, 0, lineCount() - windowLines());
 }
 
 int ScreenWindow::currentResultLine() const
@@ -249,7 +249,7 @@ bool ScreenWindow::atEndOfOutput() const
 void ScreenWindow::scrollTo(int line)
 {
     int maxCurrentLineNumber = lineCount() - windowLines();
-    line = std::clamp(line, 0, maxCurrentLineNumber);
+    line = bound(line, 0, maxCurrentLineNumber);
 
     const int delta = line - _currentLine;
     _currentLine = line;

src/TerminalDisplay.cpp
@@ -2320,8 +2320,8 @@ void TerminalDisplay::extendSelection(const QPoint& position)
     // Adjust position within text area bounds.
     const QPoint oldpos = pos;
 
-    pos.setX(std::clamp(pos.x(), textBounds.left(), textBounds.right()));
-    pos.setY(std::clamp(pos.y(), textBounds.top(), textBounds.bottom()));
+    pos.setX(bound(pos.x(), textBounds.left(), textBounds.right()));
+    pos.setY(bound(pos.y(), textBounds.top(), textBounds.bottom()));
 
     if (oldpos.y() > textBounds.bottom()) {
         linesBeyondWidget = (oldpos.y() - textBounds.bottom()) / _fontHeight;