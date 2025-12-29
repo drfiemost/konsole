4c9854de49edec16710cf8b7b5db0f78ed6b853b
Leandro Nini <drfiemost@users.noreply.github.com>
2025-12-29 16:07:07 +0100

    Remove a change accidentally slipped in e8e1b3e

--- src/main.cpp
+++ src/main.cpp
@@ -46,26 +46,6 @@ bool shouldUseNewProcess();
 // restore sessions saved by KDE.
 void restoreSession(Application& app);
 
-
- void myMessageOutput(QtMsgType type, const char *msg)
- {
-     switch (type) {
-     case QtDebugMsg:
-         fprintf(stderr, "Debug: %s\n", msg);
-         break;
-     case QtWarningMsg:
-         fprintf(stderr, "Warning: %s\n", msg);
-         break;
-     case QtCriticalMsg:
-         fprintf(stderr, "Critical: %s\n", msg);
-         break;
-     case QtFatalMsg:
-         fprintf(stderr, "Fatal: %s\n", msg);
-         abort();
-     }
- }
-
-
 // ***
 // Entry point into the Konsole terminal application.
 // ***
@@ -96,8 +76,6 @@ extern "C" int KDE_EXPORT kdemain(int argc, char** argv)
     if (!KUniqueApplication::start(startFlags)) {
         exit(0);
     }
-
-    qInstallMsgHandler(myMessageOutput);
 
     Application app;
 
